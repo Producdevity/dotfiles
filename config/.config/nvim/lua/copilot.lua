90118de4213fd81aca8914017932bb083520ce18
Yassine Gherbi <ygherbi@anwb.nl>
2023-09-11 00:01:12 +0200

    feat: Set copilot_node_command dynamically

--- config/.config/nvim/lua/copilot.lua
+++ config/.config/nvim/lua/copilot.lua
@@ -1,8 +1,8 @@
--- Set copilot_node_command
-vim.g.copilot_node_command = "~/.nvm/versions/node/v16.17.0/bin/node"
+-- Set copilot_node_command manually
+-- vim.g.copilot_node_command = "~/.nvm/versions/node/v16.17.0/bin/node"
 
 -- Set copilot_node_command dynamically
--- vim.g.copilot_node_command = vim.fn.system('which node')
+vim.g.copilot_node_command = vim.fn.trim(vim.fn.system('which node'))
 
 -- Set copilot_filetypes
 vim.g.copilot_filetypes = {
