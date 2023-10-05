5d4e3c26d495d6870f36a7baf6112ebee5494500
Yassine Gherbi <yassine@hexelnet.com>
2023-10-05 07:52:23 +0200

    feat: added pnpm and symlinks

--- node/install-global-packages.sh
+++ node/install-global-packages.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 
-npm_packages=()
-
-yarn_packages=(
-    "typescript"
-    "typescript-language-server"
-    "neovim"
+npm_packages=(
+  "yarn"
+  "pnpm"
 )
 
-echo "Installing global npm packages..."
+yarn_packages=(
+  "typescript"
+  "typescript-language-server"
+  "neovim"
+)
+
+pnpm_packages=(
+  "eas-cli"
+  "neovim"
+)
+
+echo "Installing global NPM packages..."
 for package in "${npm_packages[@]}"; do
     npm install -g "$package"
 done
@@ -16,6 +24,11 @@ done
 echo "Installing global Yarn packages..."
 for package in "${yarn_packages[@]}"; do
     yarn global add "$package"
+done
+
+echo "Installing global PNPM packages..."
+for package in "${pnpm_packages[@]}"; do
+    pnpm add -g "$package"
 done
 
 echo "🚀Installation of global packages complete."
