be354066d59a6a14547cbf64efb292d0258d098b
Yassine Gherbi <yassine@hexelnet.com>
2023-10-25 06:11:07 +0200

    feat: set fixed node version for copilot because it is behaving like a little bitch

--- config/.config/nvim/lua/copilot.lua
+++ config/.config/nvim/lua/copilot.lua
@@ -1,8 +1,8 @@
 -- Set copilot_node_command manually
--- vim.g.copilot_node_command = "~/.nvm/versions/node/v16.17.0/bin/node"
+vim.g.copilot_node_command = "~/.nvm/versions/node/v16.20.2/bin/node"
 
 -- Set copilot_node_command dynamically
-vim.g.copilot_node_command = vim.fn.trim(vim.fn.system('which node'))
+-- vim.g.copilot_node_command = vim.fn.trim(vim.fn.system('which node'))
 
 -- Set copilot_filetypes
 vim.g.copilot_filetypes = {
