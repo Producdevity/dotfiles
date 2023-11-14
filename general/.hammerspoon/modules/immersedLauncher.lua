e2f8c97c2940af1b4085dc2d68f5170ae84488d5
Yassine Gherbi <yassine@hexelnet.com>
2023-11-14 22:04:32 +0100

    refactor: split hammerspoon config into modules

---
+++ general/.hammerspoon/modules/immersedLauncher.lua
@@ -0,0 +1,17 @@
+usbWatcher = nil
+appName = "Immersed"
+-- Open and Close when Quest headset is commected and disconnected
+function usbDeviceCallback(data)
+  --   if string.match(data["productName"], "Quest") then
+  if (data["productName"] == "Quest 3" or data["productName"] == "Quest Pro") then
+    if (data["eventType"] == "added") then
+      hs.application.launchOrFocus(appName)
+    elseif (data["eventType"] == "removed") then
+      app = hs.appfinder.appFromName(appName)
+      app:kill()
+    end
+  end
+end
+
+usbWatcher = hs.usb.watcher.new(usbDeviceCallback)
+usbWatcher:start()
